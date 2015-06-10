8fce9063a47a67c4e0b2b19332d32ce3feb40e80
审判天使 <xxstop@qq.com>
2015-06-10 14:50:36 +0800

    change extends

--- LttCustomer/app/src/main/java/com/gilbert/ltt/lttcustomer/ui/user/HomeActivity.java
+++ LttCustomer/app/src/main/java/com/gilbert/ltt/lttcustomer/ui/user/HomeActivity.java
@@ -2,13 +2,13 @@ package com.gilbert.ltt.lttcustomer.ui.user;
 
 import android.os.Bundle;
 
-import com.gilbert.ltt.diana.core.RoboAppCompatActivity;
+import com.gilbert.ltt.diana.local.LttAppCompatActivity;
 import com.gilbert.ltt.lttcustomer.R;
 
 /**
  * Created by xxstop on 15/6/10.
  */
-public class HomeActivity extends RoboAppCompatActivity {
+public class HomeActivity extends LttAppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
