b91c8ca457a1c77fca942fce4f7425f0646de210
审判天使 <xxstop@qq.com>
2015-06-11 18:10:52 +0800

    add list

--- LttCustomer/app/src/main/java/com/gilbert/ltt/lttcustomer/ui/user/HomeActivity.java
+++ LttCustomer/app/src/main/java/com/gilbert/ltt/lttcustomer/ui/user/HomeActivity.java
@@ -1,14 +1,16 @@
 package com.gilbert.ltt.lttcustomer.ui.user;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.gilbert.ltt.diana.local.LttAppCompatActivity;
 import com.gilbert.ltt.lttcustomer.R;
 import com.google.inject.Inject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import roboguice.inject.InjectView;
 
@@ -19,8 +21,8 @@ public class HomeActivity extends LttAppCompatActivity {
 
     @InjectView(R.id.lv_personal_opera) ListView lvPersonalOpera;
     @InjectView(R.id.lv_contact) ListView lvContact;
-    @Inject ArrayList<String> alPersonalOpera;
-    @Inject ArrayList<String> alContact;
+    @Inject ArrayList<Map<String,String>> alPersonalOpera;
+    @Inject ArrayList<Map<String,String>> alContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,18 +40,38 @@ public class HomeActivity extends LttAppCompatActivity {
     }
 
     private void initPersonalOperaList() {
-        alPersonalOpera.add("位置");
-        alPersonalOpera.add("个人资料");
-        alPersonalOpera.add("账户与安全");
-        lvPersonalOpera.setAdapter(new ArrayAdapter<>(getApplication(),
-                android.R.layout.simple_list_item_1, alPersonalOpera));
+        alPersonalOpera.add(new HashMap<String, String>() {
+            {
+                put("title", "位置");
+            }
+        });
+        alPersonalOpera.add(new HashMap<String, String>() {
+            {
+                put("title", "个人资料");
+            }
+        });
+        alPersonalOpera.add(new HashMap<String, String>() {
+            {
+                put("title", "账户与安全");
+            }
+        });
+        lvPersonalOpera.setAdapter(new SimpleAdapter(HomeActivity.this, alPersonalOpera,
+                R.layout.user_opera_item, new String[]{"title"}, new int[]{R.id.title}));
     }
 
     private void initContactList() {
-        alContact.add("意见反馈");
-        alContact.add("客户电话");
-        lvContact.setAdapter(new ArrayAdapter<>(getApplication(),
-                android.R.layout.simple_list_item_1, alContact));
+        alContact.add(new HashMap<String, String>() {
+            {
+                put("title", "意见所反馈");
+            }
+        });
+        alContact.add(new HashMap<String, String>() {
+            {
+                put("title", "客户电话");
+            }
+        });
+        lvContact.setAdapter(new SimpleAdapter(HomeActivity.this, alContact,
+                R.layout.user_opera_item, new String[]{"title"}, new int[]{R.id.title}));
     }
 
 }
