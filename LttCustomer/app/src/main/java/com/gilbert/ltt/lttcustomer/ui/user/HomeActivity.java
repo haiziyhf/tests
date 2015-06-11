ed8c88264d6479bc121ead7a3da075202ecb1600
审判天使 <xxstop@qq.com>
2015-06-11 11:44:51 +0800

    add user list

--- LttCustomer/app/src/main/java/com/gilbert/ltt/lttcustomer/ui/user/HomeActivity.java
+++ LttCustomer/app/src/main/java/com/gilbert/ltt/lttcustomer/ui/user/HomeActivity.java
@@ -1,14 +1,26 @@
 package com.gilbert.ltt.lttcustomer.ui.user;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.gilbert.ltt.diana.local.LttAppCompatActivity;
 import com.gilbert.ltt.lttcustomer.R;
+import com.google.inject.Inject;
+
+import java.util.ArrayList;
+
+import roboguice.inject.InjectView;
 
 /**
  * Created by xxstop on 15/6/10.
  */
 public class HomeActivity extends LttAppCompatActivity {
+
+    @InjectView(R.id.lv_personal_opera) ListView lvPersonalOpera;
+    @InjectView(R.id.lv_contact) ListView lvContact;
+    @Inject ArrayList<String> alPersonalOpera;
+    @Inject ArrayList<String> alContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +30,26 @@ public class HomeActivity extends LttAppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         }
+
+        // - 初始化个性操作
+        initPersonalOperaList();
+        // - 初始化官方联系
+        initContactList();
+    }
+
+    private void initPersonalOperaList() {
+        alPersonalOpera.add("位置");
+        alPersonalOpera.add("个人资料");
+        alPersonalOpera.add("账户与安全");
+        lvPersonalOpera.setAdapter(new ArrayAdapter<>(getApplication(),
+                android.R.layout.simple_list_item_1, alPersonalOpera));
+    }
+
+    private void initContactList() {
+        alContact.add("意见反馈");
+        alContact.add("客户电话");
+        lvContact.setAdapter(new ArrayAdapter<>(getApplication(),
+                android.R.layout.simple_list_item_1, alContact));
     }
 
 }
