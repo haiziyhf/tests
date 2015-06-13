9c06421d8eaf2768c207cad5f62f60f00d654100
xxstop <xxstop@qq.com>
2015-06-13 16:16:12 +0800

    update diana && add actionbar

---
+++ LttCustomer/app/src/main/java/com/gilbert/ltt/lttcustomer/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.gilbert.ltt.lttcustomer.controller;
+
+import com.gilbert.ltt.diana.core.AbstractController;
+
+/**
+ * Created by xxstop on 2015/6/13.
+ */
+public class UserController extends AbstractController {
+}
