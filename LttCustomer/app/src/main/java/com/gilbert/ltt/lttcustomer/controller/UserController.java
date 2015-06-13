38f179683759fbd536e31883800b2b3b75e3ba79
xxstop <xxstop@qq.com>
2015-06-13 17:02:35 +0800

    update diana

--- LttCustomer/app/src/main/java/com/gilbert/ltt/lttcustomer/controller/UserController.java
+++ LttCustomer/app/src/main/java/com/gilbert/ltt/lttcustomer/controller/UserController.java
@@ -1,9 +1,9 @@
 package com.gilbert.ltt.lttcustomer.controller;
 
-import com.gilbert.ltt.diana.core.AbstractController;
+import com.gilbert.ltt.diana.local.controller.AppController;
 
 /**
  * Created by xxstop on 2015/6/13.
  */
-public class UserController extends AbstractController {
+public class UserController extends AppController {
 }
